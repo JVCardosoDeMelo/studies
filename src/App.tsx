afafcb940bd9513c2c97d3498464a1b950c17a6f
Joao Vitor cardoso <sr.melovitor@gmail.com>
2023-10-30 21:03:53 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Botao from './componentes/Botao';
+import Formulario from './componentes/Formulario';
+import Lista from './componentes/Lista';
+
+function App() {
+  return (
+    <div className="App">
+         <Formulario />
+         <Lista />
+    </div>
+  );
+}
+
+export default App;
